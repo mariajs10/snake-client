0b5a95b8da66a3a815566ef97927ffc606903d6b
Maria Jose <mariacijim@gmail.com>
2020-12-01 18:28:21 +0000

    given name to snake

--- client.js
+++ client.js
@@ -4,7 +4,14 @@ const connect = function() {
     host: 'localhost',
     port: 50541
   });
+  conn.on('connect', () => {
+    console.log('New client connected!')
+  });
+
   // interpret incoming data as text
+  conn.on('connect', () => {
+    conn.write('Name: NM');
+  });
 
   conn.setEncoding('utf8'); 
   conn.on('data', (data) => {
